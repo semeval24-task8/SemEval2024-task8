5f0674bfa794747b8bf4208cdd0704b893633548
alonscheuer <11799711+alonscheuer@users.noreply.github.com>
2023-12-15 16:31:50 +0100

    entity coherence working code

--- system/entity-coherence.py
+++ system/entity-coherence.py
@@ -1,16 +1,10 @@
-import spacy
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas
 import argparse
 from tqdm import tqdm
-import coreferee
+import stanza
 
-nlp = spacy.load('en_core_web_sm')
-nlp.add_pipe('coreferee')
-
-doc = nlp("I like Pontus. He is a good friend.")
-x = doc.doc._.coref_chains[0][0][0]
-print([i for i, s in enumerate(doc.sents) if x in range(s.start, s.end)][0])
+nlp = stanza.Pipeline("en", processors="tokenize,mwt,pos,lemma,depparse,coref")
 
 def create_arg_parser():
   parser = argparse.ArgumentParser()
@@ -20,30 +14,35 @@ def create_arg_parser():
 
 def entity_coherence(text):
   doc = nlp(text)
-  role_mapping = {
+  role_rank = {
+    "nsubj": 3,
+    "nsubjpass": 3,
+    "csubj": 3,
+    "csubjpass": 3,
+    "agent": 3,
+    "obj": 2,
+    "dative": 2
+  }
+  role_map = {
     "nsubj": "S",
     "nsubjpass": "S",
     "csubj": "S",
     "csubjpass": "S",
     "agent": "S",
-    "dobj": "O",
-    "pobj": "O",
+    "obj": "O",
     "dative": "O",
-    "_other": "X",
-    "_none": "-"
+    "-": "-"
   }
-  roles = list(role_mapping.values())
+  roles = ["S","O","X","-"]
 
-  # TODO
-  refs = doc._.coref_chains
+  chains = doc.coref
+  mentions_by_sentence = [[[m.start_word for m in c.mentions if m.sentence == s] for s in range(len(doc.sentences))] for c in chains]
+  roles_by_sentence = [[max([doc.sentences[i].words[m].deprel for m in s], key=lambda role: role_rank.get(role, 1)) if len(s) > 0 else "-" for i, s in enumerate(c)] for c in mentions_by_sentence]
+  role_seq_by_sentence = ["".join([role_map.get(s, "X") for s in c]) for c in roles_by_sentence]
 
-  # TODO
-  ref_roles = []
-
-  ref_role_seq = [''.join(r) for r in ref_roles]
   possible_transitions = [a + b for a in roles for b in roles]
-  vect = CountVectorizer(vocabulary=possible_transitions, ngram_range=(2,2), analyzer="char")
-  mat = vect.fit_transform(ref_role_seq)
+  vect = CountVectorizer(vocabulary=possible_transitions, ngram_range=(2,2), analyzer="char", lowercase=False)
+  mat = vect.fit_transform(role_seq_by_sentence)
   feature_ids = vect.vocabulary_
   dist = mat.sum(axis=0) / mat.sum()
   features = {feature: dist[0, id] for feature, id in feature_ids.items()}
@@ -55,4 +54,6 @@ def main():
   tqdm.pandas()
   data = data.join(data.progress_apply(lambda x: entity_coherence(x["text"]), axis=1, result_type="expand"))
 
-main()
+# main()
+
+print(entity_coherence("I like Pontus. He is a good friend and he knows everything. I am friendly"))
